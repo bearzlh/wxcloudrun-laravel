d0e3f0a8052530bfb603a429f451d06873edeb8a
zhenglihao <zhenglh@dianzhong.com>
2023-01-18 00:24:30 +0800

    参数修改

--- app/Http/Controllers/Wechat.php
+++ app/Http/Controllers/Wechat.php
@@ -19,6 +19,12 @@ class Wechat extends Controller
                 'headers' => [
                     'Content-Type' => 'application/json',
                     'charset' => 'utf-8'
+                ],
+                'json' => [
+                    "is_expire"=>true,
+                    "expire_type"=>1,
+                    "expire_interval"=>1,
+                    "env_version"=>"release"
                 ]
             ]);
             $result = json_decode($result->getBody(), true);
